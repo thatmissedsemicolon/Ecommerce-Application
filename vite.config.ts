140b1196df7f98d7f1076e6a06fb789b1ead4648
Suryansh <suryansh.anand@icloud.com>
2024-01-04 22:07:15 -0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/script.js',
+        assetFileNames: (assetInfo) => {
+          if (/\.(png|svg|jpg)$/.test(assetInfo.name)) {
+            return `assets/${assetInfo.name}`;
+          }        
+          return 'assets/style.css';
+        },
+      },
+    },
+  },
 })
